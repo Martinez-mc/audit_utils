9894e938d43a611d53465e6ddadb61f74130668a
Martinez <martinez@admin.com>
2025-05-14 09:02:03 -0400

    modified

--- xlwings/pd_pivot.py
+++ xlwings/pd_pivot.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = pd.read_clipboard()
 pvt = pd.pivot_table(df,
-                     index=["Payment ID (Internal)", "Check/Item Date","Supplier Number", "Invoice Number"],
+                     index=["Id)", "Check Date","Supplier Number", "Invoice Number"],
                      aggfunc={'Payment Amount': np.sum})
 
 pvt.to_excel('Pivot.xlsx')
